2a050051691ab2e005e8ac80e691c82b38b9d4a4
gblackhorn <guoda.gd@gmail.com>
2022-12-08 17:48:42 +0900

    Delete useless lines

--- Arduino_Airpuff_TTL_SyncBackToNvoke/Arduino_Airpuff_TTL_SyncBackToNvoke.ino
+++ Arduino_Airpuff_TTL_SyncBackToNvoke/Arduino_Airpuff_TTL_SyncBackToNvoke.ino
@@ -1,32 +1,22 @@
 /*
-   Modified from "Simple Arduino 4 Channel TTL Closed Loop Generator" written by:
-
-   Optogenetics and Neural Engineering Core ONE Core
-   University of Colorado
- 
-
    A TTL input is received by the Arduino, and then a series of signal is sent out on a separate channel to activate airpuff machine.
 */
 
 //This section defines the pins on the Arduino. We specify the intigers (int) TTL1, TTL2, and TTL3.
 //If you expanding this to require more than two channels, uncomment (remove "//") below.
 int TTL1 = 2;    //Input from nVoke2: If HIGH, Arduino will send varied-duration signal to airpuff machine
-int TTL2 = 4;   //The TTL signal to control the airpuff machine will come from here
+int TTL2 = 4;   //The output TTL signal to control the airpuff machine 
 int TTL3 = 7;   //Input from nVoke2: If HIGH, Arduino will send one signal (con_single) to airpuff machine 
-// int TTL3 = 7;       //The IR sensors will get powered from here
-//int TTL4 = 8;     //Not used in this example
-//int TTL5 = 12;    //Can be set as a TTL
-//int TTL6 = 13;    //Can be set as a TTL, though, not recommend, as there are slight differences in this pin
 
 //This section defines variables that the Arduino will use to define the state of the sensor
 // int sensorState = LOW, i = 0;
 int airpuff_varied = LOW;
 int airpuff_single = LOW;
 
-//Now we write the pulse train for the optogenetic stimulation.
+//Pulse train for the airpuff stimulation.
 //Draw out the desired waves for the stimulation. The condition (or state) of the Arduino changes any time any of the channels change. See the write up for more information.
-//The number of conditions is dependent on how many times the Arduino changes. For the given example, the laser is to be pulsed on for 25 ms, off for 25, on for 25, then off for 75. This pulse train is to be delivered twice for a given nose poke.
-//Define the number of conditions. Define the length of time (in ms) for each condition. [250 500 750 1000] ms
+//The number of conditions is dependent on how many times the Arduino changes. 
+//Define the number of conditions. Define the length of time (in ms) for each condition. [50 100 250 500] ms
 //Define the number of times to repeat the pulse train
 
 int con1 = 50;
@@ -42,19 +32,8 @@ int con_single = con2; //
 int  con[] = {con1, con2, con3, con4, con1, con2, con3, con4}; // conditions in ms
 int num_con = 8;
 
-// int numrepeat = 2;
-// int static_var_on = 1;
-// int static_var_off = 0;
 
-// Swap two elements in an array
-void swapEntries(int a, int b)
-{
-  int temp = con[a];
-  con[a] = con[b];
-  con[b] = temp;
-}
-
-// Print the content in 'con'
+// Print the content in 'con' for checking the function of the code
 void printData()
 {
   for (const auto& v : con) {
@@ -84,7 +63,7 @@ void setup()
 void loop() 
 {
   airpuff_varied = digitalRead(TTL1); // Read TTL1, if HIGH, start the stimulation
-  airpuff_single = digitalRead(TTL3); // Read TTL1, if HIGH, start the stimulation
+  airpuff_single = digitalRead(TTL3); // Read TTL3, if HIGH, start the stimulation
 
   // Apply varied duration airpuff stimulations
   if (airpuff_varied == HIGH)
